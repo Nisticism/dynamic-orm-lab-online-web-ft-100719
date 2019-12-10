ce0df2d393209c1213907a63ceaf3aa5882693c7
Hans Foster <fosterhans@gmail.com>
2019-12-10 05:10:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,29 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+  attr_accessor :name, :grade
+  attr_reader :id 
 
+  def initialize(id: nil, name:, grade:)
+    @id = id
+    @name = name
+    @grade = grade
+    
+    
+  end
+  
+  def self.find_by_name
+    
+  end
+  
+  def self.table_name
+    
+  end
+  
+  def self.column_names
+    
+  end
+  
+  def 
 end
